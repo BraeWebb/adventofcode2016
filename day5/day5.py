0ae3b51fa5fb409b0055ff39e21efb3a543003d0
BraeWebb <email@braewebb.com>
2016-12-12 14:23:32 +1000

    Day 5

---
+++ day5/day5.py
@@ -0,0 +1,35 @@
+import hashlib
+
+def decode(code):
+    index = 0
+    password = ''
+    while True:
+        if hashlib.md5('{}{}'.format(code, index).encode()).hexdigest()[:5] == '00000':
+            password = password + hashlib.md5('{}{}'.format(code, index).encode()).hexdigest()[5]
+            if len(password) == 8:
+                break
+        index += 1
+    return password
+
+def advanced_decode(code):
+    index = 0
+    password = [None]*8
+    while True:
+        hexcode = hashlib.md5('{}{}'.format(code, index).encode()).hexdigest()
+        if hexcode[:5] == '00000':
+            if hexcode[5].isdigit():
+                code_index = int(hexcode[5])
+                if code_index < 8 and password[code_index] == None:
+                    password[code_index] = hexcode[6]
+                    if None not in password:
+                        break
+        index += 1
+    return ''.join(password)
+
+#print(decode('abc'))
+#print(advanced_decode('abc'))
+
+with open('inputs.txt') as f:
+    code = f.readline()
+    print(decode(code))
+    print(advanced_decode(code))
